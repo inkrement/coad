d664ec0b44080bf2203e8076a8e642ee64a72fed
Christian Hotz-Behofsits <christian.hotz-behofsits@wu.ac.at>
2017-01-15 15:13:08 +0000

    update preprocess

--- preprocess.R
+++ preprocess.R
@@ -2,6 +2,8 @@ library(readr)
 library(magrittr)
 library(dplyr)
 
+# read data
+# remove unused columns
 bids <- read_csv('./data/bids.csv', col_types = list(
   Time_Bid = col_character(),
   BidID = col_skip(),
@@ -9,17 +11,11 @@ bids <- read_csv('./data/bids.csv', col_types = list(
   IP = col_skip(),
   Domain = col_skip(),
   AdslotID = col_skip(),
-  URL = col_skip()
-)) %>% mutate(
-  Time_Bid = substr(Time_Bid, 1, 12)
-) %>% write_csv('./data/bids_preprocessed.csv')
-
-bids <- read_csv('./data/bids_preprocessed.csv', col_types = list(
-  Time_Bid = col_datetime("%Y%m%d%H%M")
+  URL = col_skip(),
+  Time_Bid = col_datetime("%Y%m%d%H%M%*")
 ))
 
 ## remove unused rows
-##TODO: empty userid?
 cleanedbids <- bids %>% 
   filter(!is.na(UserID)) %>%  # we need the user id so delete all rows without one
   filter(!is.na(Time_Bid))  %>%  # same for bid time
